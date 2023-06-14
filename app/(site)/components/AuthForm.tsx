8ec86a4918253342fe247a13f87fdca6955fcd11
JFernando12 <fire.jf2012@gmail.com>
2023-06-14 14:05:21 -0600

    changes

--- app/(site)/components/AuthForm.tsx
+++ app/(site)/components/AuthForm.tsx
@@ -7,7 +7,7 @@ import { useForm, FieldValues, SubmitHandler } from 'react-hook-form';
 type Variant = 'LOGIN' | 'REGISTER';
 
 const AuthForm = () => {
-  const [variant, setVariant] = useState<Variant>('REGISTER');
+  const [variant, setVariant] = useState<Variant>('LOGIN');
 
   const {
     register,
@@ -20,6 +20,10 @@ const AuthForm = () => {
       password: '',
     },
   });
+
+  const toggleVariant = () => {
+    setVariant((prev) => (prev === 'LOGIN' ? 'REGISTER' : 'LOGIN'));
+  };
 
   return (
     <div className="sm:max-w-md sm:m-auto sm:w-full bg-white px-4 py-8 shadow sm:rounded-lg sm:px-10">
@@ -54,13 +58,26 @@ const AuthForm = () => {
           {variant === 'LOGIN' ? 'Sign in' : 'Register'}
         </Button>
       </form>
+      <div className="mt-6">
+        {/* Linea divisoria con texto */}
+        <div className="relative">
+          <div className="absolute inset-0 flex items-center">
+            <div className="border-t w-full" />
+          </div>
+          <div className="">holis</div>
+        </div>
+        {/* Botones de redes sociales */}
+        <div className="mt-10">
+          <div>icon1</div>
+        </div>
+      </div>
       <div className="flex gap-2 justify-center text-sm mt-6 px-2 text-gray-500">
         <div>
           {variant === 'LOGIN'
             ? 'New to Messenger?'
             : 'Already have an account?'}
         </div>
-        <div className="underline cursor-pointer">
+        <div onClick={toggleVariant} className="underline cursor-pointer">
           {variant === 'LOGIN' ? 'Create and account' : 'Login'}
         </div>
       </div>
